2b3bbe6259964da317a1faf7b3cad5c8a2bef2d1
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-11-25 14:03:53 +0700

    create endpoint to get users account

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,12 +2,15 @@ import express from "express"
 import userController from "../controllers/userController.js"
 
 const router = express.Router()
-const { loginUser, signupUser } = userController;
+const { loginUser, signupUser, usersAccount } = userController;
 
 //login route
 router.post('/login', loginUser)
 
 //sign up route
 router.post('/signup', signupUser)
+
+//users
+router.get('/users', usersAccount)
 
 export default router
