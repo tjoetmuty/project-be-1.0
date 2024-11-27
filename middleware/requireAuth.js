4752631d1ad2467fd56debfe9c4c584c3055ce53
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-11-27 16:56:35 +0700

    create middleware for authorization

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,4 +1,23 @@
-const requireAuth = (req, res, next) => {
+import userModel from "../src/models/userModel.js"
+import jwt from "jsonwebtoken"
+
+const requireAuth = async (req, res, next) => {
   //verify auth
   const {authorization } = req.headers
-} 
+  if(!authorization){
+    return res.status(401).json({error: 'auth token required'})
+  }
+
+  const token = authorization.split(' ')[1]
+  console.log("auth token nih: ", token)
+
+  try{
+    const {_id} = jwt.verify(token, process.env.SECRET)
+    req.user = await userModel.findOne({_id}).select('_id')
+    next()
+  } catch(err) {
+    res.status(401).json({error: 'req is not authorized'})
+  }
+}
+
+export default requireAuth
